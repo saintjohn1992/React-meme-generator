81a3d01beaa8cb77df402ca7e3d1f2a24856bb35
saintjohn1992 <saintjohnshane@outlook.com>
2022-06-19 00:17:36 +0200

    created memedata.js and mapping through it to fetch img data

--- meme-generator/src/components/form/Meme.js
+++ meme-generator/src/components/form/Meme.js
@@ -1,20 +1,28 @@
-import "./meme.css"
+import "./meme.css";
+import MemeData from "../../MemeData.js";
+import {BsFillImageFill} from "react-icons/bs";
 
 
 const Meme = () => {
 
+    function getMemeImage () {
+        const memesArray = MemeData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+    }
+
     return (
     <main>
-        <form className="form">
+        <div className="form">
             
             <input type="text" className="form-input" placeholder="top-text" required={true} />
     
             <input type="text" className="form-input" placeholder="bottom-text" required={true} />
 
-            <button className="form-button">Get a new meme image</button>
+            <button className="form-button" onClick = {getMemeImage}>Get a new meme image <BsFillImageFill /></button>
         
             
-        </form>
+        </div>
     </main>    
     )
 }
